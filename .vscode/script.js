94b780be50b8a484a54dba89f27d9430374e5679
Antonio Gama <vagner.sociedadealternativa@outlook.com>
2023-11-02 20:51:40 -0300

    initial commit

---
+++ .vscode/script.js
@@ -0,0 +1,15 @@
+function toogleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //if (html.classList.contains("light")) {
+  // html.classList.remove("light")
+  //} else {
+  //  html.classList.add("light")
+  // }
+
+  // pegar a tag img
+  // substituir a imagem
+  // se tiver light mode, adicionar a imagem light
+  // se tiver sem light mode, manter a imagem normal
+}
